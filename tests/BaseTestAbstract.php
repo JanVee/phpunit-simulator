5e174d804840a1ac2d7f7a00e90e8e30b1b0021f
jiangan <jiangan@37.com>
2022-06-14 15:03:37 +0800

    feat: composer setting.

---
+++ tests/BaseTestAbstract.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author:      Jan
+ * @Description:
+ * @Date:        Created in 2022/6/14 2:46 下午
+ * @Modifid By:
+ */
+
+namespace DataProvider\Tests;
+
+
+abstract class BaseTestAbstract extends \DataProvider\BaseTestAbstract
+{
+    /**
+     * namespace of the testing suite
+     */
+    protected $testNamespace = __NAMESPACE__;
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author:      Jan
+ * @Description:
+ * @Date:        Created in 2022/6/14 2:45 下午
+ * @Modifid By:
+ */
+
+namespace DataProvider;
+
+use function error_reporting;
+
+error_reporting(E_DEPRECATED | E_WARNING | E_ERROR | E_PARSE);
+
+/* include */
+include_once('.' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+include_once('BaseTestAbstract.php');
